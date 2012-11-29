aafe39e21f32b8bab2609b8242a472e4d10166d9
albert-lopez <alopez@ac.upc.edu>
2012-11-29 18:57:02 +0100

    fix segmentation fault in nonces

--- lispd/lispd_nonce.c
+++ lispd/lispd_nonce.c
@@ -87,6 +87,8 @@ nonces_list *new_nonces_list()
 
 int check_nonce(nonces_list   *nonces, uint64_t nonce){
     int i;
+    if (nonces == NULL)
+        return (BAD);
     for (i=0;i<nonces->retransmits;i++){
         if (nonces->nonce[i] == nonce){
             free(nonces);
