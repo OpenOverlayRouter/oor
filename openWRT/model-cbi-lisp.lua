26fb56bb1c1f335e5dd8736531872c9f4fd592a5
albert-lopez <alopez@ac.upc.edu>
2013-02-27 16:44:50 +0100

    Modification of luci file to show all interfaces of the openWRT router

--- openWRT/model-cbi-lisp.lua
+++ openWRT/model-cbi-lisp.lua
@@ -5,12 +5,10 @@ s = m:section(TypedSection, "map-resolver", "Map Resolver")
 
 s = m:section(TypedSection, "map-server", "Map Server")      
    s:option(Value, "address", "Address", "Register to this map server")   
-   s:option(Value, "key-type", "Key Type")   
-   s:option(Value, "key", "Key")   
-   mode = s:option(ListValue, "verify", translate("Verify"))   
+   mode = s:option(ListValue, "key_type", translate("Key Type"))   
       mode.override_values = true   
-      mode:value("on", translate("on"))   
-      mode:value("off", translate("off"))   
+      mode:value("1", translate("HMAC-SHA-1-96"))
+   s:option(Value, "key", "Key")
    mode = s:option(ListValue, "proxy_reply", translate("Proxy Reply"))   
       mode.override_values = true   
       mode:value("on", translate("on"))   
@@ -22,13 +20,16 @@ s = m:section(TypedSection, "proxy-etr", "Proxy ETR")
    s:option(Value, "weight", "Weight") 
    
 s = m:section(TypedSection, "database-mapping", "Database Mapping")      
-   s:option(Value, "eid_prefix", "EID prefix", "IPv4 EID of the OpenWrt LISP node")   
-   mode = s:option(ListValue, "interface", translate("Interface"))   
-      mode.override_values = true   
-      mode:value("eth0", translate("eth0"))   
-      mode:value("wlan0", translate("wlan0"))   
-   s:option(Value, "priority", "Priority")  
-   s:option(Value, "weight", "Weight")
+   s:option(Value, "eid_prefix", "EID prefix", "IPv4 or IPv6 EID of the OpenWrt LISP node")   
+   ni = s:option(Value, "interface", translate("RLOC Interface"), "Interface containing the RLOCs associated to this EID")
+   	ni.template    = "cbi/network_ifacelist"
+   	ni.override_values = true   
+   	ni.widget = "radio"
+   	ni.nobridges = false
+   s:option(Value, "priority_v4", "IPv4 Priority")  
+   s:option(Value, "weight_v4", "IPv4 Weight")
+   s:option(Value, "priority_v6", "IPv6 Priority")  
+   s:option(Value, "weight_v6", "IPv6 Weight")
    
 s = m:section(TypedSection, "proxy-itr", "Proxy ITR")      
    s:option(Value, "address", "Address", "List of PITRs to SMR on handover ") 
