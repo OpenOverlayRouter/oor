c08e7c3c33abde0d4c8399f54df6466eff139117
Oriol Mari <oriolmarimarques@gmail.com>
2018-06-26 16:35:14 +0200

    Solved an issue with handover

---
+++ oor/data-plane/apple/ios/ios_input.h
@@ -0,0 +1,29 @@
+/*
+ *
+ * Copyright (C) 2011, 2015 Cisco Systems, Inc.
+ * Copyright (C) 2015 CBA research group, Technical University of Catalonia.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at:
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *
+ */
+
+
+#ifndef IOS_INPUT_H_
+#define IOS_INPUT_H_
+
+#include "../../../lib/sockets.h"
+
+int ios_process_input_packet(sock_t *sl);
+int ios_rtr_process_input_packet(sock_t *sl);
+
+#endif /* ios_INPUT_H_ */
---
+++ oor/net_mgr/apple/ios/netm_ios.h
@@ -0,0 +1,16 @@
+//
+//  netm_ios.h
+//  oor-iOS
+//
+//  Created by Oriol Marí Marqués on 29/06/2017.
+//  Copyright © 2017 Oriol Marí Marqués. All rights reserved.
+//
+
+#ifndef netm_ios_h
+#define netm_ios_h
+
+#include <stdio.h>
+
+int getdefaultgateway(in_addr_t * addr, char *TypeEN);
+
+#endif /* netm_ios_h */
